2b7e438497ba7a533379267678200600246b5da7
Jake Miller <doulosjakem@gmail.com>
2023-08-28 13:23:10 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+include 'template.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Web Page</title>
+</head>
+<body>
+	<header>
+		<h1>Welcome to My Web Page</h1>
+	</header>
+	<main>
+		<p>This is the main content of the web page.</p>
+	</main>
+	<footer>
+		<p>© 2023 My Company</p>
+	</footer>
+</body>
+</html>
