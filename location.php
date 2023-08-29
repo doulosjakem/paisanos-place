f9d44774b4e534686475327339da21ada1c99876
Jake Miller <doulosjakem@gmail.com>
2023-08-29 09:46:24 -0600

    location first draft completed

--- location.php
+++ location.php
@@ -8,8 +8,25 @@
 <?php  include "TopNav.php" ?>   
 	<main>
     <div class="container-fluid PPtan text-center border-bottom border-danger border-5 py-4">
-        <h2>Location</h2>
-</div>
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col align-items-center">
+            <h2>Location</h2>
+            <a class="text-decoration-none" href="https://www.google.com/maps/search/300+N+South+St.+Boise,+ID+83333/@43.6171055,-116.240856,13z/data=!3m1!4b1?hl=en-US&entry=ttu">
+                <p>300 N South St. Boise, ID 83333</p>
+            </a>
+            <div class="mapouter mx-auto rounded"><div class="gmap_canvas"><iframe width="350" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=300%20N%20South%20St%20Boise%20ID&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-to.org"></a><br><style>.mapouter{position:relative;text-align:right;height:400px;width:350px;}</style><a href="https://www.embedgooglemap.net">google maps embed</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:400px;width:350px;}</style></div></div>
+            </div>
+            <div class="col-1"></div>
+        </div>        
+    </div>
+    <div class="container-fluid PPdred text-center border-bottom border-danger border-5 py-4">
+        <a class="text-decoration-none" href="https://www.google.com/maps/search/300+N+South+St.+Boise,+ID+83333/@43.6171055,-116.240856,13z/data=!3m1!4b1?hl=en-US&entry=ttu">
+            <h2 class="PPwhitetxt">Come on in!</h2>
+                <p class="PPwhite text-shadow rounded mx-5">300 N South St. Boise, ID 83333</p>            
+            <img class="img-fluid rounded" src="images/building.png" alt="logo of paisanos, which is a chef in front of an italian flag, seen on a sign on a wall in a busy city">
+            </a>
+    </div>
 	</main>
 <?php  include "BotFooter.php" ?>
 </body>
