4e6767c460c9f97768d78b5a8dcafc91acba80ec
Jake Miller <doulosjakem@gmail.com>
2023-09-01 14:15:45 -0600

    Changes to placing order

--- menu.js
+++ menu.js
@@ -3,24 +3,70 @@ $(document).ready(function () {
   $spaghetti = 0;
   $chickenFettucini = 0;
   $totalItems = 0;
+
+  $lasagnaPrice = 12.99;
+  $spaghettiPrice = 10.99;
+  $chickenFettuciniPrice = 11.99;
+  $totalPrice = 0;
+
   $displayTxt = "There are no items in the cart. ";
   updateCartTxt();
+  initialPrices();
 
-  function updateCartTxt() {
-    $displayTxt = "";
-    if ($lasagna > 0) {
-      $displayTxt += $lasagna + " Order(s) of Lasagna. ";
-    }
-    if ($spaghetti > 0) {
-      $displayTxt += $spaghetti + " Order(s) of Spaghetti. ";
-    }
-    if ($chickenFettucini > 0) {
-      $displayTxt += $chickenFettucini + " Order(s) of Chicken Fettucini. ";
-    }
+  function initialPrices() {
+    $("#lasagnaInitial").text("$" + $lasagnaPrice);
+    $("#spaghettiInitial").text("$" + $spaghettiPrice);
+    $("#chickenFettuciniInitial").text("$" + $chickenFettuciniPrice);
+    countUpdate();
+  }
+
+  // function updateCartTxt() {
+  //   $displayTxt = "";
+  //   if ($lasagna > 0) {
+  //     $displayTxt += $lasagna + " Order(s) of Lasagna. ";
+  //   }
+  //   if ($spaghetti > 0) {
+  //     $displayTxt += $spaghetti + " Order(s) of Spaghetti. ";
+  //   }
+  //   if ($chickenFettucini > 0) {
+  //     $displayTxt += $chickenFettucini + " Order(s) of Chicken Fettucini. ";
+  //   }
+  //   if ($lasagna == 0 && $spaghetti == 0 && $chickenFettucini == 0) {
+  //     $displayTxt = "There are no items in the cart. ";
+  //   }
+  //   $("#cartTxt").text($displayTxt);
+  // }
+
+  function updateCartPage() {
+    $("#lasagnaInitialCart").text("$" + $lasagnaPrice);
+    $("#spaghettiInitialCart").text("$" + $spaghettiPrice);
+    $("#chickenFettuciniInitialCart").text("$" + $chickenFettuciniPrice);
+
     if ($lasagna == 0 && $spaghetti == 0 && $chickenFettucini == 0) {
-      $displayTxt = "There are no items in the cart. ";
+      $("#cartWarning").removeClass("d-none");
+    } else {
+      $("#cartWarning").addClass("d-none");
+      $("#cartWarning").removeClass("d-none");
     }
-    $("#cartTxt").text($displayTxt);
+
+    //displays each only if there are some in the cart
+    if ($lasagna > 0) {
+      $("#lasagnaCartRow").removeClass("d-none");
+    } else {
+      $("#lasagnaCartRow").addClass("d-none");
+    }
+
+    if ($spaghetti > 0) {
+      $("#spaghettiCartRow").removeClass("d-none");
+    } else {
+      $("#spaghettiCartRow").addClass("d-none");
+    }
+
+    if ($chickenFettucini > 0) {
+      $("#chickenFettuciniCartRow").removeClass("d-none");
+    } else {
+      $("#chickenFettuciniCartRow").addClass("d-none");
+    }
   }
 
   function countUpdate() {
@@ -29,7 +75,22 @@ $(document).ready(function () {
     $("#chickenFettuciniAmt").text($chickenFettucini + " in cart");
     $totalItems = $lasagna + $spaghetti + $chickenFettucini;
     $("#totalAmt").text($totalItems + " in cart");
-    updateCartTxt();
+    updateCartPage();
+    priceUpdate();
+  }
+
+  function priceUpdate() {
+    $("#lasagnaPrice").text("$" + $lasagna * $lasagnaPrice);
+    $("#spaghettiPrice").text("$" + $spaghetti * $spaghettiPrice);
+    $("#chickenFettuciniPrice").text(
+      "$" + $chickenFettucini * $chickenFettuciniPrice
+    );
+    $totalPrice = 0;
+    $totalPrice =
+      $lasagna * $lasagnaPrice +
+      $spaghetti * $spaghettiPrice +
+      $chickenFettucini * $chickenFettuciniPrice;
+    $("#totalPrice").text("$" + $totalPrice + " Subtotal");
   }
 
   //   Lasagna
@@ -66,5 +127,14 @@ $(document).ready(function () {
       $chickenFettucini--;
     }
     countUpdate();
+  });
+
+  // Place Order
+  $("#placeOrder").click(function () {
+    if ($totalItems > 0) {
+      alert("Your order has been placed.");
+    } else {
+      alert("You did not order anything.");
+    }
   });
 });
--- menu.php
+++ menu.php
@@ -29,7 +29,9 @@
                             </thead>
                             <tbody class="PPgreen rounded">
                                 <tr class=" PPgreen">
-                                    <td>Lasagna</td>
+                                    <td>Lasagna
+                                        <p id="lasagnaInitial"></p>
+                                    </td>
                                     <td>
                                         <img width="150px" class="img-fluid rounded" src="images/lasagna.jpg"
                                             alt="lasagna on a plate with a fork">
@@ -42,15 +44,21 @@
                                         <button id="addLasagna" type="button" class="btn btn-primary"
                                             data-bs-toggle="button" autocomplete="off">Add to Cart</button>
                                         <br>
-                                        <p class="mt-3 rounded PPred text-white" id="lasagnaAmt">0 in
-                                            cart
-                                        </p>
+                                        <div class="my-3 rounded PPred text-white">
+                                            <div class="" id="lasagnaAmt">0 in
+                                                cart
+                                            </div>
+                                            <div id="lasagnaPrice"></div>
+                                        </div>
+
                                         <button id="removeLasagna" type="button" class="btn btn-primary"
                                             data-bs-toggle="button" autocomplete="off">Remove from Cart</button>
                                     </td>
                                 </tr>
                                 <tr class="rounded">
-                                    <td>Spaghetti</td>
+                                    <td>Spaghetti
+                                        <p id="spaghettiInitial">$</p>
+                                    </td>
                                     <td>
                                         <img width="150px" class="img-fluid rounded" src="images/spaghetti.jpg"
                                             alt="spaghetti in a bowl">
@@ -64,15 +72,20 @@
                                         <button id="addSpaghetti" type="button" class="btn btn-primary"
                                             data-bs-toggle="button" autocomplete="off">Add to Cart</button>
                                         <br>
-                                        <p class="mt-3 rounded PPred text-white" id="spaghettiAmt">0 in
-                                            cart
-                                        </p>
+                                        <div class="my-3 rounded PPred text-white">
+                                            <div class="" id="spaghettiAmt">0 in
+                                                cart
+                                            </div>
+                                            <div id="spaghettiPrice"></div>
+                                        </div>
                                         <button id="removeSpaghetti" type="button" class="btn btn-primary"
                                             data-bs-toggle="button" autocomplete="off">Remove from Cart</button>
                                     </td>
                                 </tr>
                                 <tr class="rounded">
-                                    <td>Chicken Fettucini</td>
+                                    <td>Chicken Fettucini
+                                        <p id="chickenFettuciniInitial">$</p>
+                                    </td>
                                     <td>
                                         <img width="150px" class="img-fluid rounded" src="images/chickenFettucini.jpg"
                                             alt="chicken fettucini in a bowl">
@@ -86,9 +99,12 @@
                                         <button id="addChickenFettucini" type="button" class="btn btn-primary"
                                             data-bs-toggle="button" autocomplete="off">Add to Cart</button>
                                         <br>
-                                        <p class="mt-3 rounded PPred text-white" id="chickenFettuciniAmt">0 in
-                                            cart
-                                        </p>
+                                        <div class="my-3 rounded PPred text-white">
+                                            <div class="" id="chickenFettuciniAmt">0 in
+                                                cart
+                                            </div>
+                                            <div id="chickenFettuciniPrice"></div>
+                                        </div>
                                         <button id="removeChickenFettucini" type="button" class="btn btn-primary"
                                             data-bs-toggle="button" autocomplete="off">Remove from Cart</button>
                                     </td>
@@ -100,8 +116,10 @@
                         <p class="p-2 rounded PPred text-white" id="totalAmt">0 in
                             cart
                         </p>
-                        <button class="btn btn-primary" data-bs-target="#checkoutModal" data-bs-toggle="modal"
-                            data-bs-dismiss="modal">Checkout</button>
+                        <p class="p-2 rounded PPred text-white" id="totalPrice">$0 Subtotal
+                        </p>
+                        <p class="p-2 rounded btn btn-primary text-white" data-bs-target="#checkoutModal"
+                            data-bs-toggle="modal" data-bs-dismiss="modal">Checkout</p>
                     </div>
                 </div>
             </div>
@@ -115,37 +133,106 @@
                     </div>
                     <div class="modal-body PPtan rounded m-1">
                         <h4>Your Cart</h4>
-                        <div id="cartTxt"></div>
+                        <p id="cartWarning" class="d-none">There are no items in the cart.</p>
+                        <table class="table table-striped table-bordered rounded">
+                            <thead>
+                            </thead>
+                            <tbody class="PPgreen rounded">
+                                <tr id="lasagnaCartRow" class="d-none">
+                                    <td>Lasagna
+                                        <p id="lasagnaInitialCart"></p>
+                                    </td>
+                                    <td>
+                                        <img width="150px" class="img-fluid rounded" src="images/lasagna.jpg"
+                                            alt="lasagna on a plate with a fork">
+                                        <br>
+                                        <small>
+                                            Photo by Dana Tentis: https://www.pexels.com/photo/salted-pasta-725990/
+                                        </small>
+                                    </td>
+                                    <td>
+                                        <button id="addLasagna" type="button" class="btn btn-primary"
+                                            data-bs-toggle="button" autocomplete="off">Add to Cart</button>
+                                        <br>
+                                        <div class="my-3 rounded PPred text-white">
+                                            <div class="" id="lasagnaAmt">0 in
+                                                cart
+                                            </div>
+                                            <div id="lasagnaPrice"></div>
+                                        </div>
+
+                                        <button id="removeLasagna" type="button" class="btn btn-primary"
+                                            data-bs-toggle="button" autocomplete="off">Remove from Cart</button>
+                                    </td>
+                                </tr>
+                                <tr id="spaghettiCartRow" class="d-none">
+                                    <td>Spaghetti
+                                        <p id="spaghettiInitialCart">$</p>
+                                    </td>
+                                    <td>
+                                        <img width="150px" class="img-fluid rounded" src="images/spaghetti.jpg"
+                                            alt="spaghetti in a bowl">
+                                        <br>
+                                        <small>
+                                            Photo by Anna Guerrero:
+                                            https://www.pexels.com/photo/lasagna-on-white-ceramic-plate-4079520/
+                                        </small>
+                                    </td>
+                                    <td>
+                                        <button id="addSpaghetti" type="button" class="btn btn-primary"
+                                            data-bs-toggle="button" autocomplete="off">Add to Cart</button>
+                                        <br>
+                                        <div class="my-3 rounded PPred text-white">
+                                            <div class="" id="spaghettiAmt">0 in
+                                                cart
+                                            </div>
+                                            <div id="spaghettiPrice"></div>
+                                        </div>
+                                        <button id="removeSpaghetti" type="button" class="btn btn-primary"
+                                            data-bs-toggle="button" autocomplete="off">Remove from Cart</button>
+                                    </td>
+                                </tr>
+                                <tr id="chickenFettuciniCartRow" class="d-none">
+                                    <td>Chicken Fettucini
+                                        <p id="chickenFettuciniInitialCart">$</p>
+                                    </td>
+                                    <td>
+                                        <img width="150px" class="img-fluid rounded" src="images/chickenFettucini.jpg"
+                                            alt="chicken fettucini in a bowl">
+                                        <br>
+                                        <small>
+                                            Photo by Engin Akyurt:
+                                            https://www.pexels.com/photo/oval-white-bowl-2697229/
+                                        </small>
+                                    </td>
+                                    <td>
+                                        <button id="addChickenFettucini" type="button" class="btn btn-primary"
+                                            data-bs-toggle="button" autocomplete="off">Add to Cart</button>
+                                        <br>
+                                        <div class="my-3 rounded PPred text-white">
+                                            <div class="" id="chickenFettuciniAmt">0 in
+                                                cart
+                                            </div>
+                                            <div id="chickenFettuciniPrice"></div>
+                                        </div>
+                                        <button id="removeChickenFettucini" type="button" class="btn btn-primary"
+                                            data-bs-toggle="button" autocomplete="off">Remove from Cart</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                     <div class="modal-footer">
                         <button class="btn PPredHov PPred text-white" data-bs-target="#buildCartModal"
                             data-bs-toggle="modal" data-bs-dismiss="modal">Back to Build Your Cart</button>
-                        <button class="btn btn-primary" data-bs-target="#paymentModal" data-bs-toggle="modal"
-                            data-bs-dismiss="modal">Proceed to Payment</button>
+                        <button id="placeOrder" class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-dismiss="modal">Place
+                            Order</button>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="modal fade" id="paymentModal" aria-hidden="true" aria-labelledby="PaymentButton" tabindex="-1">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content PPgreen">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="PaymentButton">Payment</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body PPtan rounded m-1">
-                        <h4>Your Cart</h4>
-                        <div id="cartTxt"></div>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn PPredHov PPred text-white" data-bs-target="#buildCartModal"
-                            data-bs-toggle="modal" data-bs-dismiss="modal">Back to Build Your Cart</button>
-                        <button class="btn btn-primary" data-bs-target="#paymentModal" data-bs-toggle="modal"
-                            data-bs-dismiss="modal">Proceed to Payment</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+
         <div class="container-fluid PPblack PPwhitetxt text-center border-bottom border-danger border-5 py-4">
             <h2>Our Menu</h2>
             <a class="btn btn-primary my-2" data-bs-toggle="modal" href="#buildCartModal" role="button">Open
